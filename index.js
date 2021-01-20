9dfaf107586469c18d14cc6176036b01ebb89c94
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-19 21:44:47 -0600

    commit tree 68a1a1cd15c8d530067d5342c91aff199e510235

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+} 
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+} 
+
+function wrapAdjective(arg) {
+    const inner = function(message){
+        return `You are ${arg}${message}${arg}!`
+    }
+    return inner 
+}
+
+
+const Calculator = {
+     add : function(a,b) {
+        return a + b
+     }, 
+     multiply : function(a,b){
+         return a*b
+     },
+     subtract : function(a,b){
+         return a - b
+     },
+     divide : function(a,b){
+         return a/b
+     }
+}
+
+function actionApplyer(num1, funcs){
+    let numCopy = num1
+    if (funcs.length == 0){
+        return num1
+    }
+    funcs.forEach(func => numCopy = func(numCopy))
+    return numCopy
+}
+
